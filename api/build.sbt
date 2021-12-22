d4438eb4a295780209faee125a539e7fa4743fa2
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-12-22 20:57:02 +0000

    Apped api dir

---
+++ api/build.sbt
@@ -0,0 +1,12 @@
+ThisBuild / scalaVersion := "2.13.6"
+
+val eventStore = (project in file("./event-store"))
+  .settings(moduleName := "event-store")
+  .settings(
+    libraryDependencies ++= Seq(
+      Dependencies.http4sDsl,
+      Dependencies.http4sBlazeServer,
+      Dependencies.http4sBlazeClient,
+      Dependencies.catsEffect
+    )
+  )
---
+++ api/project/Dependencies.scala
@@ -0,0 +1,13 @@
+import sbt._
+object Dependencies {
+  private val http4sVersion     = "0.23.7"
+  private val catsEffectVersion = "3.3.1"
+
+  private def http4s(branch: String): ModuleID =
+    "org.http4s" %% s"http4s-$branch" % http4sVersion
+
+  val http4sDsl         = http4s("dsl")
+  val http4sBlazeServer = http4s("blaze-server")
+  val http4sBlazeClient = http4s("blaze-client")
+  val catsEffect        = "org.typelevel" %% "cats-effect" % catsEffectVersion
+}
