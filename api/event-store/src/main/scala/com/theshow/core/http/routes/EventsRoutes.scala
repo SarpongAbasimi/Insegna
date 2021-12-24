8657dad868addbd00a518d38ce3f6ddc414dc938
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-12-24 17:47:27 +0000

    Modified Routes to accept a post request

--- api/event-store/src/main/scala/com/theshow/core/http/routes/EventsRoutes.scala
+++ api/event-store/src/main/scala/com/theshow/core/http/routes/EventsRoutes.scala
@@ -1,14 +1,24 @@
 package com.theshow.core.http.routes
 
 import cats.effect.kernel.Async
+import com.theshow.core.domain.Event
 import org.http4s.HttpRoutes
+import org.http4s.circe.jsonOf
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.Router
 
 case class EventsRoutes[F[_]: Async]() extends Http4sDsl[F] {
   private[routes] val prefix = "/api/v1/event"
-  private val routes: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root =>
-    Ok("Hello")
+
+  private val routes: HttpRoutes[F] = HttpRoutes.of[F] { case req @ POST -> Root =>
+    implicit val entityDecoder = jsonOf[F, Event]
+
+    req
+      .attemptAs[Event]
+      .foldF(
+        _ => BadRequest("And error occurred check the request body"),
+        _ => Created("Created")
+      )
   }
 
   val router = Router(prefix -> routes)
