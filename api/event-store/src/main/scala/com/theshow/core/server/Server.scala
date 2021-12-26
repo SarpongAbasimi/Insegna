445deb3eb8153c268859e29fc319378cf9ec3a72
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-12-26 21:59:46 +0000

    Added Algebra for kafka consumer and producers

--- api/event-store/src/main/scala/com/theshow/core/server/Server.scala
+++ api/event-store/src/main/scala/com/theshow/core/server/Server.scala
@@ -5,18 +5,26 @@ import cats.effect.kernel.Async
 import cats.effect.std.Console
 import com.theshow.core.config.Config
 import com.theshow.core.http.routes.EventsRoutes
+import com.theshow.core.kafka.{KafkaConsumerAlgebra, KafkaProducerAlgebra}
 import org.http4s.blaze.server.BlazeServerBuilder
 import fs2.Stream
 
 object Server {
   def stream[F[_]: Async: Console](config: Config): fs2.Stream[F, ExitCode] = for {
     _ <- Stream.eval(Console[F].println("Starting the server 🚀"))
+
+    kafkaProducerAlgebra: KafkaProducerAlgebra[F] = KafkaProducerAlgebra
+      .impl[F](config.kafkaConfig)
+    kafkaConsumerAlgebra: KafkaConsumerAlgebra[F] = KafkaConsumerAlgebra
+      .impl[F](config.kafkaConfig)
+
     sever <- BlazeServerBuilder[F]
       .bindHttp(
         config.serverConfig.port.value,
         config.serverConfig.host.value
       )
-      .withHttpApp(EventsRoutes[F].router.orNotFound)
+      .withHttpApp(EventsRoutes[F](kafkaProducerAlgebra).router.orNotFound)
       .serve
+      .concurrently(kafkaConsumerAlgebra.consume)
   } yield sever
 }
