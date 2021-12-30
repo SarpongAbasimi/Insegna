7b9be1a65aeacdaed986098b9425557bae2f2b5b
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-12-30 16:31:45 +0000

    Passed RestHightLevelClient to the server

--- api/event-store/src/main/scala/com/theshow/core/Main.scala
+++ api/event-store/src/main/scala/com/theshow/core/Main.scala
@@ -2,12 +2,14 @@ package com.theshow.core
 
 import cats.effect.{ExitCode, IO, IOApp}
 import com.theshow.core.config.Config
+import com.theshow.core.elasticsearch.{ElasticClientAlgebra, EsAlgebra}
 import com.theshow.core.server.Server
 import fs2.Stream
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = (for {
-    config <- Stream.eval(Config.config[IO].load)
-    server <- Server.stream[IO](config)
+    config         <- Stream.eval(Config.config[IO].load)
+    restHighClient <- Stream.resource(ElasticClientAlgebra.impl[IO](config.esConfig).client)
+    server         <- Server.stream[IO](config, restHighClient)
   } yield server).compile.drain.as(ExitCode.Success)
 }
