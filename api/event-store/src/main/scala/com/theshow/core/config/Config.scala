a22b3af828b9b077ae0000d6d3622b641e701636
Sarpong Abasimi <abasimisarpong@rocketmail.com>
2021-12-30 16:33:42 +0000

    Added esConfig to Config

--- api/event-store/src/main/scala/com/theshow/core/config/Config.scala
+++ api/event-store/src/main/scala/com/theshow/core/config/Config.scala
@@ -6,11 +6,12 @@ import cats.implicits._
 
 final case class Config(
     serverConfig: ServerConfig,
-    kafkaConfig: KafkaConfig
+    kafkaConfig: KafkaConfig,
+    esConfig: EsConfig
 )
 
 object Config {
   def config[F[_]: Async]: ConfigValue[F, Config] =
-    (ServerConfig.serverConfig[F], KafkaConfig.kafkaConfig[F])
-      .parMapN((serverConfig, kafkaConfig) => Config(serverConfig, kafkaConfig))
+    (ServerConfig.serverConfig[F], KafkaConfig.kafkaConfig[F], EsConfig.esConfig[F])
+      .parMapN((serverConfig, kafkaConfig, esConfig) => Config(serverConfig, kafkaConfig, esConfig))
 }
