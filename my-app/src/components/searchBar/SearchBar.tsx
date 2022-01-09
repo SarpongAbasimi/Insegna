2dd59e22041f583d14c7be0ebad02e0ba2480c9d
sarpong <abasimisarpong@rocketmail.com>
2022-01-09 20:38:01 +0000

    Modified searchBar

--- my-app/src/components/searchBar/SearchBar.tsx
+++ my-app/src/components/searchBar/SearchBar.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import styles from './SearchBar.module.scss'
+import styles from './SearchBar.module.scss';
+import {SerchBarProp} from '../../utils/utls';
 
-
-export const SearchBar = () => {
+export const SearchBar = ({placeHolder}: SerchBarProp) => {
     return (
       <form className={styles.search}>
-        <input type='search' placeholder="Search your thoughts 💬 " className={styles.children}/>
+        <input type='search' placeholder={placeHolder} className={styles.children}/>
         <input type='submit' className={styles.submit}/>
       </form>
     )
